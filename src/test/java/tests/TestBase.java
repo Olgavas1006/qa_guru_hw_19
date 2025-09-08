8a17084c65413b00dc3c6412f8a91322f7b4998b
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-09-08 10:09:36 +0500

    update JunitTest

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,7 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import config.WebDriverProvider;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
-
 
 public class TestBase {
 
